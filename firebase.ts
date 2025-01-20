4515b7080ca57fcc48cd47f5fadfb86d1190ed5b
AntonewtonQ <antonewtonquima@gmail.com>
2025-01-20 01:57:50 +0100

    variaveis do ambiente

--- firebase.ts
+++ firebase.ts
@@ -4,12 +4,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCM1WMa8o1HowQOqiPAS9ia5InyckGRA-Y",
-  authDomain: "illustrablog-db.firebaseapp.com",
-  projectId: "illustrablog-db",
-  storageBucket: "illustrablog-db.firebasestorage.app",
-  messagingSenderId: "992365338539",
-  appId: "1:992365338539:web:b86f5d6ee14e5ad1c0fcee"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
